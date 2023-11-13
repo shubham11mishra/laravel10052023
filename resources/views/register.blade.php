b4fc1575b45799c96087f4e1f9799545cecf160d
shubham mishra <shubham@fininfocom.com>
2023-11-13 17:51:52 +0530

    register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,27 @@
+<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+      <div class="mx-auto max-w-md text-center">
+        <a href="/" class="flex justify-center">
+          <img class="h-10" src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/original_logo_0DliJcfsTcciZen38gX9.png" alt="DEV Community" />
+        </a>
+        <div class="divide-y divide-gray-300/50">
+          <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+            <p>DEV Community is a community of 1,188,403 amazing developers</p>
+            <a class="flex cursor-pointer items-center justify-start rounded-md border-2 p-2 shadow-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-hidden="true" class="crayons-icon crayons-icon--default">
+                <path d="M3 3h18a1 1 0 011 1v16a1 1 0 01-1 1H3a1 1 0 01-1-1V4a1 1 0 011-1zm9.06 8.683L5.648 6.238 4.353 7.762l7.72 6.555 7.581-6.56-1.308-1.513-6.285 5.439h-.001z"></path>
+              </svg>
+              <span class="pl-5"> Signup with email.</span>
+            </a>
+            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+          </div>
+          <div class="pt-8 text-base font-semibold leading-7">
+            <p class="text-gray-900">Already have an account?</p>
+            <p>
+              <a href="https://tailwindcss.com/docs" class="text-sky-500 hover:text-sky-600">Login</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
--- routes/auth.php
+++ routes/auth.php
@@ -9,7 +9,12 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function (){
+    Route::get('/enter',[RegisterController::class,'index'])->name('enter');
+});
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
